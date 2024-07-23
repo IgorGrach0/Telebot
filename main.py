faeb3784a79b81f62b0eb2f89aac2762b9162aa7
IgorGrach0 <igor.grach.00@mail.ru>
2024-07-23 08:27:21 +0500

    second

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import telebot
 from telebot import types
 import sqlite3
 import record
+import sqlite3
 
+conn = sqlite3.connect('users.db')
+cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS users
+                     (ID TEXT, login TEXT, balance INTEGER)''')
+conn.commit()
+conn.close()
 # Создание соединения с SQLite3 базой данных
 
  # Создание таблицы
@@ -12,13 +19,13 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id,'Привет, я игровой бот, для начала новой игры введите команду  /start_new_game. \n '
-                                     'Для продолжения введите команду  /continue_game.')
-@bot.message_handler(commands=['start_new_game'])
-def button_message(message):
-    global status
-    status = 'login'
-    bot.send_message(message.chat.id,'Введите имя пользователя')
+    check = record.search('ID', message.chat.id)
+    if check == True:
+        bot.send_message(message.chat.id, 'Вы уже зарегистрированы')
+    else:
+        global status
+        status = 'login'
+        bot.send_message(message.chat.id, 'Вы  ещё не зарегистрированы. \nВведите имя пользователя:')
 
 
 @bot.message_handler(content_types=['text'])
@@ -28,13 +35,14 @@ def handle_text(message):
         import sqlite3
         b = 10000
         ID = message.chat.id
-        conn = sqlite3.connect('../users.db')
+        conn = sqlite3.connect('users.db')
         cur = conn.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS users
                      (ID TEXT, login TEXT, balance INTEGER)''')
         cur.execute("INSERT INTO users VALUES (?, ?, ?)", (ID, message.text, b))
         conn.commit()
         conn.close()
+        bot.send_message(message.chat.id, 'Вы успешно зарегистрированы')
 
 
 if __name__ == '__main__':
--- record.py
+++ record.py
@@ -1,8 +1,10 @@
-def registration(ID):
+def search(column, text):
     import sqlite3
-    b = 10000
-    conn = sqlite3.connect('../users.db')
-    cur = conn.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS users
-             (login TEXT, balance INTEGER)''')
-    cur.execute("INSERT INTO users VALUES (?, ?)", (ID, b))
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute(f"SELECT * FROM users WHERE {column} = ?", (text, ))
+    searching = c.fetchall()
+    if searching:
+        return True
+    else:
+        return False
